ebde45de58426c5efa4198b71bf2d46137d9adfb
Dawid Sokol <dawidsokol@CF1.local>
2019-05-29 10:26:09 +0100

    zmiany

---
+++ WpfApplication8/MailHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Threading;
+using System.Timers;
+
+namespace WpfApplication8
+{
+    class MailHandler
+    {
+        public void SetTimer()
+        {
+            System.Timers.Timer aTimer = new System.Timers.Timer(2000);
+
+            aTimer.Elapsed += SendMail;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private static void SendMail(Object source, ElapsedEventArgs e)
+        {
+            Console.Write("dsadsad");
+        }
+    }
+}
