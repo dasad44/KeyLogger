99dbd4d4cd3d2f4fc520348d0da9930582fa4488
Axlii50 <34549741+Axlii50@users.noreply.github.com>
2019-06-17 14:37:58 +0200

    AddingToRegistry
    Dodawanie Aplikacji do rejestru (Autorun)

---
+++ AddingToRegistry.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication8
+{
+    class AddingToRegistry
+    {
+        RegistryKey rk;
+        string AppName;
+        public AddingToRegistry(string AppName)
+        {
+            rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            this.AppName = AppName;
+        } 
+        public void AddToRegistry()
+        {
+            rk.SetValue(AppName, System.Reflection.Assembly.GetEntryAssembly().Location);
+        }
+        public void RemoveFromRegistry()
+        {
+            rk.DeleteValue(AppName, false);
+        }
+    }
+}
